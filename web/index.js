ef5e5bc25816f3e22fd526fc1761a251c8515e41
laoli <407907175@qq.com>
2018-01-24 14:37:15 +0800

    静态页面

--- web/index.js
+++ web/index.js
@@ -1,3 +1,18 @@
-$('form').serializeJSON();
+(function(){
+
+    var editor = CodeMirror.fromTextArea(document.getElementById("J_json_editor"), {
+        lineNumbers: true,
+        mode: "application/json",
+        matchBrackets: true,
+        smartIndent: true,
+        indentUnit: 4,
+        tabSize: 4,
+        theme: "eclipse"
+    });
+
+    editor.setSize("100%", 400);
+
+    
 
 
+})()
